532ff2d9436b1cd7517775f1b7c86cb3dabd3748
@Dev-Muhammad-Junaid <Dev.muhammadjunaid@gmail.com>
2021-12-30 22:25:01 +0500

    Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:build_i_t/home_page/home_page_widget.dart';
 import 'flutter_flow/flutter_flow_theme.dart';
 import 'flutter_flow/flutter_flow_theme.dart';
 
+//Hello mateen
 void main() async {
   runApp(MyApp());
 }
